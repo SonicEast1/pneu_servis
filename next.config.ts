db765af38c5f0f85331820b9f45da85a4cbca436
Filip <karasek.filip@ssakhk.cz>
2026-06-30 15:49:55 +0200

    fuuuua

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  productionBrowserSourceMaps: false,
+
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+    memoryBasedWorkersCount: false,
+
+    webpackBuildWorker: false,
+    webpackMemoryOptimizations: true,
+    parallelServerCompiles: false,
+    parallelServerBuildTraces: false,
+
+    staticGenerationRetryCount: 1,
+    staticGenerationMaxConcurrency: 1,
+    staticGenerationMinPagesPerWorker: 1000,
+  },
 };
 
 export default nextConfig;
